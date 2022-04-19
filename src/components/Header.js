549d5051060626044d5bd7a6b1dbf1f1e61d12f1
Calvin Tan <calvintan607@gmail.com>
2022-04-18 17:09:33 -0700

    added logo image in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../css/Header.css'
+import logo from '../images/covid_image.png'
+
 
 export default function Header(){
     return(
         <div className="header">
+            <img className = "logo" src={logo}/>
             <p className='headerText'>United States Covid Statistics Map</p>
         </div>
     )
