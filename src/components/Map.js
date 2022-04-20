a1d93c3b4bf045709f0dba67147230304185d459
Calvin Tan <calvintan607@gmail.com>
2022-04-20 11:13:41 -0700

    axios works on click with CA state

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {geoCentroid} from 'd3-geo'
+import axios from 'axios'
 
 
 
@@ -12,7 +13,15 @@ import {
     ZoomableGroup,
 } from 'react-simple-maps'
 
-
+function stateAbbreviation(state){
+    const name = state.name
+    console.log(name)
+    if(name=="California"){
+        return "CA"
+    } else if (name=="Colorado"){
+        return "CO"
+    }
+}
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json"
 
@@ -31,7 +40,29 @@ const [content,setContent] = useState('')
                             geography = {geo} 
                             stroke ="#FFF" 
                             fill="#DDD"
-                            
+                            /*
+                            onMouseLeave={
+                                setContent('')
+                            }
+                            */
+                            onMouseDownCapture={()=>{
+                                //console.log(stateAbbreviation(geo.properties))
+                                //console.log(stateAbbreviation(geo.properties))
+                                const abbreviation = stateAbbreviation(geo.properties)
+                                var url = `https://data.cdc.gov/resource/9mfq-cb36.json?$limit=1&state=${abbreviation}&$order=submission_date%20DESC`
+                                console.log(url)
+                              axios.get(`https://data.cdc.gov/resource/9mfq-cb36.json?$limit=1&state=${abbreviation}&$order=submission_date%20DESC`)
+                                .then(res => {
+                                  console.log(res.data)
+                                  setContent(res.data)
+                                  //console.log(content.tot_cases)
+                                  //console.log(res.data)
+                                  
+                                }).catch(err => {
+                                  console.log(err)
+                                }) 
+                                
+                            }}
                             />
                         ))
                     }
