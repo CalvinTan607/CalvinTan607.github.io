46b1a2561db6404070e129ffcd91724641944c6a
Calvin Tan <calvintan607@gmail.com>
2022-04-20 12:46:39 -0700

    added the rest of the states abbreviations

--- src/components/Map.js
+++ src/components/Map.js
@@ -15,13 +15,59 @@ import {
 
 function stateAbbreviation(state){
     const stateName = state.name;
-
+    console.log(stateName)
     const abbreviation = {
-        Arizona: "AZ",
-        California: "CA",
-        Colorado: "CO",
-    };
-
+        "Arizona": "AZ",
+        "Alabama": "AL",
+        "Alaska": "AK",
+        "Arkansas": "AR",
+        "California": "CA",
+        "Colorado": "CO",
+        "Connecticut": "CT",
+        "Delaware": "DE",
+        "Florida": 'FL',
+        "Georgia":"GA",
+        "Hawaii": "HI",
+        "Idaho": "ID",
+        "Illinois": "IL",
+        "Indiana": 'IN',
+        "Iowa": 'IA',
+        "Kansas": 'KS',
+        "Kentucky": 'KY',
+        "Louisiana": 'LA',
+        "Maine": 'ME',
+        "Maryland": 'MD',
+        "Massachusetts": 'MA',
+        "Michigan": 'MI',
+        "Minnesota": 'MN',
+        "Mississippi": 'MS',
+        "Missouri": 'MO',
+        "Montana": 'MT',
+        "Nebraska": 'NE',
+        "Nevada": 'NV',
+        "New Hampshire": 'NH',
+        'New Jersey': 'NJ',
+        'New Mexico': 'NM',
+        'New York': 'NY',
+        'North Carolina': 'NC',
+        'North Dakota': 'ND',
+        'Ohio': 'OH',
+        'Oklahoma': 'OK',
+        'Oregon': 'OR',
+        'Pennsylvania': 'PA',
+        'Rhode Island': 'RI',
+        'South Carolina': 'SC',
+        'South Dakota': 'SD',
+        'Tennessee': 'TN',
+        'Texas': 'TX',
+        'Utah': 'UT',
+        'Vermont': 'VT',
+        'Virginia': 'VA',
+        'Washington': 'WA',
+        'West Virginia': 'WV',
+        'Wisconsin': 'WI',
+        'Wyoming': 'WY',
+    }
     return abbreviation[`${stateName}`]
 }
 
@@ -34,7 +80,7 @@ const [content,setContent] = useState('')
         <div>
             <ComposableMap projection="geoAlbersUsa">
             
-                <Geographies geography={geoUrl}>
+            <Geographies geography={geoUrl}>
                     {({geographies})=>
                         geographies.map((geo)=>(
                             <Geography 
@@ -69,6 +115,8 @@ const [content,setContent] = useState('')
                         ))
                     }
                 </Geographies>
+            
+
                 
             </ComposableMap>
         </div>
