2a1d296ba14c33b0f5c30f8dcdd4a2d4ef0217c7
Calvin Tan <calvintan607@gmail.com>
2022-04-26 14:46:37 -0700

    baseline card css added,very ugly

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,11 @@ import '../css/Card.css'
 export default function Card(props){
     return(
         <div className="card">
-            <h1>{props.state}</h1>
+            <h3 className="stateName">{props.state}</h3> 
+                <div className='infoSection'>
+                    <p className='statistics'>Total Cases: {props.tot_cases}</p>
+                    <p className='statistics'>Total Deaths: {props.tot_death}</p>
+                </div>
         </div>
     )
 }
