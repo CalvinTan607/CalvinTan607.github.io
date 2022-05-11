92621f8496cccf025314fc19c9289f3857183a36
Calvin Tan <calvintan607@gmail.com>
2022-05-11 13:49:23 -0700

    routing is was avaliable last commit, but is now managed in Views.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 import {useState} from 'react'
 import ReactTooltip from 'react-tooltip'
@@ -6,20 +6,17 @@ import ReactTooltip from 'react-tooltip'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Views from './components/Views'
-import Data from './components/Data'
-import MapWithToolTip from './components/MapWithToolTip';
+
 
 
 
 import './css/App.css'
 
 function App() {
-  const [content,setContent] = useState('');
   return (
       <div className="AppContainer">
         <Header />
-        <Data/>
-        <MapWithToolTip/>
+        <Views/>
         <Footer />
       </div>
   );
--- src/components/Views.js
+++ src/components/Views.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import {Route,Routes} from 'react-router-dom'
 
+import MapWithToolTip from './MapWithToolTip'
+import Data from './Data'
+
 
 export default function Views(){
     return (
-        <h1>Views</h1>
+    <Routes>
+        <Route path ='/' element = {<MapWithToolTip/>}/>
+        <Route path ='/Data' element = {<Data/>}/>
+    </Routes>
     )
 }
